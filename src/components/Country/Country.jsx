2aa215820c787a086ab1b1481dde06178c1db894
Hikmat <hikmatbk101@gmail.com>
2020-11-04 06:02:55 -0800

    Modified Search FilterDropDown

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,10 @@
 import React,{useState, useEffect} from 'react';
-import {FormControl, NativeSelect} from '@material-ui/core'
+import {FormControl, NativeSelect,TextField} from '@material-ui/core'
 import {fetchCountries} from '../../api'
  import styles from './Country.module.css'
-
+ import Autocomplete from '@material-ui/lab/Autocomplete';
 const Country=({handelCountryChange})=>{
     const[country, setCountry]= useState([])
-
      useEffect(()=>{
        const fetchCountryAPI=async ()=>{
            setCountry(await fetchCountries() )
@@ -14,14 +13,29 @@ const Country=({handelCountryChange})=>{
        fetchCountryAPI();
      },[setCountry])
      console.log(country)
+    
     return(<div>
         <FormControl className={styles.FormControl}>
-            <NativeSelect defaultValue="" onChange={(e)=>handelCountryChange(e.target.value)}>
+            {/* <NativeSelect defaultValue="" onChange={(e)=>handelCountryChange(e.target.value)}>
             <option value="">Global</option>
             {country.map((countries, i)=><option key={i} value={countries}>{countries} </option>)}
-            </NativeSelect>
+            </NativeSelect> */}
+
+            <Autocomplete
+            id="debug"
+            options={country}
+           // getOptionLabel= {(option) => option.title}
+          onChange={(event,country) => handelCountryChange(country)}
+          // onSelect={(event) => handelCountryChange(event)}
+           style={{ width: 300 }}
+            debug
+            renderInput={(params) => <TextField {...params} label="Global" margin="normal"/>}
+            />
+
+            
         </FormControl>
     </div>)
+    
 }
 
 export default Country
